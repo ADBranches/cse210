135ddf0c5dcf6b44aac8c58c3333a0de97aa19a6
ADBranches <bambiedwins03@gmail.com>
2025-07-27 00:47:30 +0300

    modified:   Program.cs 	new file:   Reference.cs 	new file:   Scripture.cs 	new file:   Word.cs

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        // sample scripture (John 3:16)
+        Reference reference = new Reference("John", 1, 1);
+        Scripture scripture = new Scripture(reference, "in the beginning was the word, and the word was with God, and the word was God. and the word became man.");
+
+        string userInput = "";
+
+        while (userInput != "quit" && !scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress enter to continue or type 'quit' to finish:");
+            userInput = Console.ReadLine();
+
+            scripture.HideRandomWords(3);
+        }
     }
 }
