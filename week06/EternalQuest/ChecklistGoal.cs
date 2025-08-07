5ae098c8f097ac5aa655d287c8bb04dd3d9528fe
ADBranches <bambiedwins03@gmail.com>
2025-08-07 22:38:07 +0300

    modified:   ChecklistGoal.cs

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,40 @@
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    // Constructor
+    public ChecklistGoal(string name, string description, int points, int target, int bonus)
+        : base(name, description, points)
+    {
+        _target = target;
+        _bonus = bonus;
+        _amountCompleted = 0;
+    }
+
+    // Overridden methods
+    public override void RecordEvent()
+    {
+        _amountCompleted++;
+        if (_amountCompleted >= _target)
+        {
+            _points += _bonus; 
+        }
+    }
+
+    public override bool IsComplete()
+    {
+        return _amountCompleted >= _target;
+    }
+
+    public override string GetDetailsString()
+    {
+        return $"[{(IsComplete() ? "X" : " ")}] {_shortName}: {_description} (Completed {_amountCompleted}/{_target})";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"ChecklistGoal:{_shortName},{_description},{_points},{_bonus},{_target},{_amountCompleted}";
+    }
+}
