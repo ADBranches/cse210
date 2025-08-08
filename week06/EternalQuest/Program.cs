0b388b0083ec5371a07e2cf9a6e600bcfd4d5804
ADBranches <bambiedwins03@gmail.com>
2025-08-08 21:28:50 +0300

    modified:   Program.cs

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,3 +1,8 @@
+/* MORE ADDED REQUIREMENTS...
+ * 1. Added negative goals (deduct points for bad habits).
+ * 2. Level-up system: Celebrate every 1000 points with ASCII art.
+ * 3. Prevented duplicate goal names to avoid confusion.
+ */
 class Program
 {
     static void Main()
